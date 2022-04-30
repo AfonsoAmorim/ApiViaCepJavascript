80b63aa0ccc78ef6e0affcaa725195702f0fc8df
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-04-29 21:05:10 -0300

    exibindo resultados

--- api.js
+++ api.js
@@ -7,7 +7,24 @@ function consultaCep(){
     let url = `https://viacep.com.br/ws/${cep}/json/`;
 
     fetch(url).then(function(response){
-        console.log(response);
+        response.json().then(function(data){
+            console.log(data);
+            mostrarEndereco(data);
+        })
     });
 }
 
+
+function mostrarEndereco(dados){
+
+    let resultado = document.querySelector('#resultado');
+    if (dados.erro){
+        resultado.innerHTML = "Endereço não encontrado"
+    }else {
+    resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+    <p>Complemento: ${dados.complemento}</p>
+    <p>Bairro: ${dados.bairro}</p>
+    <p>Cidade: ${dados.localidade} - Estado: ${dados.uf}</p>`
+}
+
+}
